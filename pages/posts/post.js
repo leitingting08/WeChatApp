db1d82bdd9b1756155a18d9a820bb1426d18996c
leitingting08 <leitingting08@gmail.com>
2018-02-04 23:00:52 +0800

    post

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,6 +1,10 @@
 // pages/posts/post.js
 Page({
-
+  imgUrls: [
+'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+    'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+  ],
   /**
    * 页面的初始数据
    */
